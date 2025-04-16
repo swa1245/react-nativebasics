5dd50d786811f5dcaf7a3907be01cadb258b628b
swa1245 <swarajpatil2123@gmail.com>
2025-04-16 22:44:52 +0530

    done

--- App.jsx
+++ App.jsx
@@ -1,26 +1,53 @@
-import {Button, SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
-import React from 'react';
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
   // const theme = useColorScheme();
   // const isdarkMode = theme === 'dark';
   // const backgroundColor = isdarkMode?"black":"white"
 
+  const [text, SetText] = useState('');
+  const [submit,SetSubmit] = useState('')
+
+  const handleSubmit = ()=>{
+    SetSubmit(text)
+    SetText('')
+  }
+
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={{
+        marginTop: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
       <View>
-        <TextInput placeholder='enter ur name' style={StyleSheet.input}/>
+        <TextInput
+          placeholder="enter ur name"
+          value={text}
+          style={styles.input}
+          onChangeText={(value)=>SetText(value)}
+        />
       </View>
-      <Button title='submit'></Button>
+      <Button title="submit" onPress={handleSubmit}></Button>
+
+      {submit ? <Text style={{marginTop: 20}}>Result: {submit}</Text> : null}
     </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
-    input:{
-      width:"100%",
-      padding:5,
-      borderWidth:1,
-      borderRadius:5
-    }
+  input: {
+    width: '100%',
+    padding: 50,
+    // borderColor:'black',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
 });
